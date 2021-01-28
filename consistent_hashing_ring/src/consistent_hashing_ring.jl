2361bda4662547054787156e8d897e96fe3f4ece
vutr <me@vutr.io>
2021-01-28 15:48:19 +0700

    Update

--- consistent_hashing_ring/src/consistent_hashing_ring.jl
+++ consistent_hashing_ring/src/consistent_hashing_ring.jl
@@ -1,6 +1,15 @@
 module consistent_hashing_ring
+using DBInterface, MySQL
 
-hash_num(x) = mod(x, 5)
-println("===============", hash_num(23))
+host, usr, pwd, db = "localhost", "user", "123123123", "mydb"
+protocol = MySQL.API.MYSQL_PROTOCOL_TCP
+ports = [3300 + i for i in range(0, length=2)]
+println(ports)
+
+make_conn(port) = DBInterface.connect(MySQL.Connection, host, usr, pwd, db=db, port=port, protocol=protocol)
+DSS = Dict(i => make_conn(ports[i]) for i in range(1, length=length(ports)))
+
+db_exec(conn, sql) = println(DBInterface.execute(conn::MySQL.Connection, sql))
+db_exec(DSS[1], "select 1 + 1;")
 
 end
