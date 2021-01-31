13c17e10328a906774d8fc387b63a04928506515
vutr <me@vutr.io>
2021-01-31 11:28:15 +0700

    update algorithm, fix angle division

--- consistent_hashing_ring/src/consistent_hashing_ring.jl
+++ consistent_hashing_ring/src/consistent_hashing_ring.jl
@@ -24,27 +24,27 @@ make_servers(count) = map(Server, 1:count)
 
 function place_servers_over_ring(servers::Array{Server})
     count = length(servers)
-    deg_block = 360 / count
+    deg_block = (2 / count)π
     place(idx) = [(idx - 1) * deg_block, servers[idx].id]
     map(place, 1:count)
 end
 
 function derive_labels(server_ring::Array, label_count::Integer)
     count = length(server_ring)
-    deg_block = 360 / count
-    step_deg = 360 / count / label_count
-    distribute(s) = map(i -> [s[1] + i * step_deg, s[2]] , 1:label_count)
+    deg_block = (2 / count)π
+    step_deg = (2 / count / label_count)π
+    distribute(s) = map(i -> [s[1] + i * count * step_deg, s[2]] , 1:label_count)
     map(distribute, server_ring)
 end
 
 
 # RUNNING ==============================================================
 # ----------- config
-server_count = 3
-label_count = 1
+server_count = 5
+label_count = 8
 
 # ----------- setup
-servers = make_servers(3)
+servers = make_servers(server_count)
 ring = place_servers_over_ring(servers)
 labels = derive_labels(ring, label_count)
 color_generator = Iterators.Stateful(distinguishable_colors(server_count))
