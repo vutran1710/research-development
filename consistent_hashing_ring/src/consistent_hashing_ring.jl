543ba848ff65542527a1a6958ea14dd9e126fc0b
vutr <me@vutr.io>
2021-02-04 13:54:17 +0700

    interactive

--- consistent_hashing_ring/src/consistent_hashing_ring.jl
+++ consistent_hashing_ring/src/consistent_hashing_ring.jl
@@ -8,12 +8,12 @@ using Colors
 
 pyplot()
 
-# ----------- config
+# ------------ config
 label_multiplier = 4
 cache_count = 3
 record_count = 100
 
-# ----------- setup
+# ------------ setup
 rec = create_records(record_count)
 store = PersistentStorage(rec)
 caches = create_cache_servers(cache_count)
@@ -21,11 +21,11 @@ ch_table = consistent_hashing(caches, label_multiplier)
 color_generator = Iterators.Stateful(distinguishable_colors(cache_count))
 color_map = Dict(s.id => popfirst!(color_generator) for s ∈ caches)
 
-# # ----------- plotting setups
+# ------------ plotting setups
 default(legendfontsize=16, framestyle=:zerolines, tickfont=(12, :white))
 plot(sin, cos, 0, 2π, aspect_ratio=1, show=true, label=false, reuse=true)
 
-# # ----------- plotting servers
+# ------------ plotting servers
 for (server_id, angles) in ch_table.server_map
     x_series, y_series = map(sin, angles), map(cos, angles)
     color = color_map[server_id]
@@ -40,17 +40,24 @@ for (server_id, angles) in ch_table.server_map
 end
 
 # NOTE: keeping the plotting-window open until user provide some input
+pin_object() = begin
+    for sample_id in 1:10
+        hashed = hashing_oject(sample_id)
+        scatter!(
+            [sin(hashed)],
+            [cos(hashed)],
+            markersize=8,
+            c=:red,
+        )
+        sleep(1)
+    end
+end
 
-for sample_id in 1:10
-    hashed = hashing_oject(sample_id)
-    scatter!(
-        [sin(hashed)],
-        [cos(hashed)],
-        markersize=8,
-        label="loc-$(sample_id)",
-        c=:red,
-    )
-    sleep(0.1)
+while true
+    command = readline()
+    if command == "add"
+        pin_object()
+    end
 end
 
 
