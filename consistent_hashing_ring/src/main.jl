b20fca4921bf19b40dd8a3ac161f8c5cc458cc8d
vutr <me@vutr.io>
2021-03-04 19:16:42 +0700

    update

--- consistent_hashing_ring/src/main.jl
+++ consistent_hashing_ring/src/main.jl
@@ -9,29 +9,26 @@ using SplitApplyCombine: group
 using Colors
 
 include("structs.jl")
-include("architech.jl")
-include("plotf.jl")
-# include("cli.jl")
+include("consistent_hashing_ring.jl")
+include("cli.jl")
 
 
 logger = SimpleLogger()
 global_logger(logger)
-
-
-# ClientCLI(
-#     "Re-construct a new System",
-#     "new" => (new_system, [Integer, Integer, Integer]),
-#     "Get a single record by its ID",
-#     "get" => (system.api__get_record, Integer),
-#     "Add a number of records to Store",
-#     "add" => (system.api__add_records, Integer),
-#     "Inspect a cache's bucket by its cache-id",
-#     "bucket" => (get_bucket, String),
-# )
-
 system = construct(10, 4, 20)
-@info system.api__get_record(2)
-@info system.api__add_records(4)
-@info system.api__get_record(2)
+
+new_system(record_count::Integer, server_count::Integer, label_count::Integer) = begin
+    system = construct(record_count, server_count, label_count)
+end
+
+
+ClientCLI(
+    "Re-construct a new System",
+    "new" => (new_system, [Integer, Integer, Integer]),
+    "Get a single record by its ID",
+    "get" => (system.api__get_record, Integer),
+    "Add a number of records to Store",
+    "add" => (system.api__add_records, Integer),
+)
 
 end
