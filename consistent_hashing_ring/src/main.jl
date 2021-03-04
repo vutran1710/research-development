3160913d5d8701dc1a21fdf26182ba3cf9cfef1d
vutr <me@vutr.io>
2021-03-04 19:05:10 +0700

    not gonna use julia plot

--- consistent_hashing_ring/src/main.jl
+++ consistent_hashing_ring/src/main.jl
@@ -5,109 +5,17 @@ using TypedTables
 using PrettyPrinting
 using UUIDs: uuid1
 using Faker: first_name, last_name
-using Test
+using SplitApplyCombine: group
+using Colors
 
 include("structs.jl")
 include("architech.jl")
-# include("plotf.jl")
+include("plotf.jl")
 # include("cli.jl")
-
-# using Plots
-# using Colors
 
 
 logger = SimpleLogger()
 global_logger(logger)
-
-# pyplot()
-
-# # ------------ setup
-# color_generator = Iterators.Stateful(distinguishable_colors(cache_count))
-# color_map = Dict(s.id => popfirst!(color_generator) for s ∈ caches)
-
-# # ------------ plotting setups
-# default(legendfontsize=16, framestyle=:zerolines, tickfont=(12, :white))
-# plot(sin, cos, 0, 2π, aspect_ratio=1, show=true, label=false, size = (700, 700))
-
-# # ------------ plotting servers
-# for (server_id, angles) in hash_table.server_map
-#     x_series, y_series = map(sin, angles), map(cos, angles)
-#     color = color_map[server_id]
-
-#     scatter!(
-#         x_series,
-#         y_series,
-#         markersize=20,
-#         label=server_id,
-#         c=color,
-#     )
-# end
-
-
-# # NOTE: keeping the plotting-window open until user provide some input
-# pin_object(table::ConsistentHashingTable) = begin
-#     for _ in 1:10
-#         hashed = hashing_oject(rand(1:1000))
-#         _, angle = locate_cache(table, hashed)
-
-#         # NOTE: animation if possible, otherwise, fuck it!
-#         # @gif for i ∈ hashed:(abs(angle-hashed)/10):angle
-#         #     plot!([sin(i)], [cos(i)], c=:black, arrow=true, label=false, linewidth=3)
-#         # end every 2
-
-#         plot!(
-#             [sin(hashed), sin(angle)],
-#             [cos(hashed), cos(angle)],
-#             arrow=1.5,
-#             label=false,
-#             c=:red,
-#             linewidth=2,
-#             linealpha=0.3,
-#         )
-#         sleep(0.1)
-#     end
-# end
-
-
-# add_points = (_...) -> 1
-
-
-# get_data = params -> begin
-#     record_id = parse(Int64, params[1])
-#     the_system.query(record_id)
-# end
-
-# compose(
-#     run_forever,
-#     handle_user_input,
-#     CLIMaster,
-# )(add_points, get_data, get_bucket)
-
-# system = construct(10, 3, 5)
-# println("Init a System with 10 records - 3 cache-servers - 5 label-replicas")
-
-# # Composing api handlers
-# new_system = (a::Integer, b::Integer, c::Integer) -> begin
-#     global system
-#     println("Re-construct a whole new System")
-#     println("--- $(a) records")
-#     println("--- $(b) cache-servers")
-#     println("--- $(c) label-replicas")
-#     system = construct(a, b, c)
-#     return nothing
-# end
-
-# get_bucket = cache_id -> begin
-#     resp = system.inspect__cache_data(cache_id)
-
-#     if resp.message == NOT_FOUND
-#         resp = system.inspect__cache_ids()
-#         @warn "Cache_id=$(cache_id) doesnt exist.\nAvailable IDS are $(resp.data)"
-#         return nothing
-#     end
-
-#     return resp.data
-# end
 
 
 # ClientCLI(
@@ -121,10 +29,9 @@ global_logger(logger)
 #     "bucket" => (get_bucket, String),
 # )
 
-system = construct(10, 4, 5)
+system = construct(10, 4, 20)
 @info system.api__get_record(2)
 @info system.api__add_records(4)
 @info system.api__get_record(2)
-
 
 end
--- consistent_hashing_ring/src/plotf.jl
+++ consistent_hashing_ring/src/plotf.jl
@@ -1,3 +1,39 @@
+layout_setup() = begin
+    set_default_plot_size(20cm, 20cm)
+    coord = Coord.cartesian(xmin=-1.5, xmax=1.5, ymin=-1.5, ymax=1.5, aspect_ratio=1)
+    circle_funcs = [x -> sqrt(1 - x^2), x -> -sqrt(1 - x^2)]
+    p = plot(circle_funcs, -1, 1, coord, color=["ring", "ring"])
+    return p
+end
+
+plot_cache_labels(cache_hash_table::Table) = begin
+    """ Using cache hash table to plot labels over the ring
+    Cache-hash-table::TypedTables to be used
+    _______________________________________
+    | label  | angle  | server   | online  |
+    ----------------------------------------
+    | String | Degree | ServerID | Boolean |
+    """
+    groups = group(getproperty(:server), cache_hash_table)
+    layers = []
+    color_generator = Iterators.Stateful(distinguishable_colors(length(cache_hash_table)))
+    color_map = Dict(r.server => popfirst!(color_generator) for r ∈ cache_hash_table)
+
+    for id in keys(groups)
+        table = groups[id]
+        labels = table.label
+        angles = table.angle
+        rad_angles = map(deg2rad, angles)
+        xes = map(cos, rad_angles)
+        yes = map(sin, rad_angles)
+        color = color_map[id]
+        points = layer(x=xes, y=yes, color=[color], Geom.point)
+        push!(layers, points)
+    end
+    layers
+end
+
+
 function distance(p1::Point, p2::Point)::Float64
     dx = (p1.x - p2.x) ^ 2
     dy = (p1.y - p2.y) ^ 2
